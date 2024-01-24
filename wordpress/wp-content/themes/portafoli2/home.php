5442462755e9bb7478ac56272c25aab744516815
izanhurtado <ihurtado@ifp-alguero.org>
2024-01-24 20:52:35 +0100

    izanhurtadooooooo

---
+++ wordpress/wp-content/themes/portafoli2/home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfoli Izan</title>
+
+    <link rel="stylesheet" type="text/css"   media="all" href="<?php   bloginfo( 'template_url' ); ?>" />
+    <link rel="stylesheet" type="text/css"   media="all" href="<?php   bloginfo( 'template_url' ); ?>/css/style.css" />
+
+    
+    
+</head>
+
+<body>
+
+    <ul>
+        <a href="mp08_UF1_01_hurtadoi/" class="un">
+            <li class="animacio">ACTIVITAT 1</li>
+        </a>
+        <a href="mp08_UF1_02_hurtadoi/index.html" class="dos">
+            <li class="animacio">ACTIVITAT 2</li>
+        </a>
+        <a href="mp08_UF1_03_feedly_hurtadoi/index.html" class="tres">
+            <li class="animacio">FEEDLY</li>
+        </a>
+        <a href="mp08_UF1_04_ifpagram_hurtadoi/index.html" class="quatre">
+            <li class="animacio">IFPAGRAM</li>
+        </a>
+        <a href="mp08_UF1_05_escacs_hurtadoi/index.html" class="cinc">
+            <li class="animacio">ESCACS</li>
+        </a>
+        <a href="mp08_UF1_06_absolute_hurtadoi/index.html" class="sis">
+            <li class="animacio">ABSOLUTE</li>
+        </a>
+        <a href="mp08_UF1_07_floretes_hurtadoi/html/index.html" class="set">
+            <li class="animacio">FLORETES</li>
+        </a>
+        <a href="mp08_UF1_08_cara_hurtadoi/html/index.html" class="vuit">
+            <li class="animacio">CARA</li>
+        </a>
+        <a href="mp08_UF1_e1_pe.lys_hurtadoi/html/index.html" class="nou">
+            <li class="animacio">EXÀMEN PEL·LIS</li>
+        </a>
+    </ul>
+
+    <div id="inici" class="fantasma">
+
+    </div>
+
+</body>
+
+</html>
---
+++ wordpress/wp-content/themes/portafoli2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfoli Izan</title>
+    <link rel="stylesheet" type="text/css"  media="all" href="<?php   bloginfo( 'template_url' ); ?>" />
+    <?php wp_redirect( "home.php#inici", 301 );?>
+    
+</head>
+
+<body>
+
+</body>
+
+</html>
